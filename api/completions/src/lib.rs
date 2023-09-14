adc669ec342dc5afaf7b2ef2aa18b66487058022
Justin Pflueger <justin.pflueger@fermyon.com>
2023-09-13 19:11:32 -0600

    more cleanup and fighting the borrow checker. i both loathe and appreciate rust at the same time.

--- api/completions/src/lib.rs
+++ api/completions/src/lib.rs
@@ -1,21 +1,9 @@
-use anyhow::{Result, bail};
-use shared::{GenerationRequestBuilder, GenerationRequest, GenerationMessage, GenerationResponseBuilder};
+use anyhow::Result;
+use shared::{GenerationRequest, GenerationResponseBuilder, InferSdkBuilder, llama2_prompt::Llama2PromptBuilder};
 use spin_sdk::{
     http::{Params, Request, Response, Router},
     http_component,
 };
-
-// constants for inferencing option defaults
-// const DEFAULT_MAX_TOKENS: u32 = 75;
-// const DEFAULT_REPEAT_PENALTY: f32 = 1.1;
-// const DEFAULT_REPEAT_PENALTY_LAST_N_TOKEN_COUNT: u32 = 64;
-// const DEFAULT_TEMPERATURE: f32 = 0.0;
-// const DEFAULT_TOP_K: u32 = 40;
-// const DEFAULT_TOP_P: f32 = 0.9;
-// const DEFAULT_MODEL: &str = "llama2-chat";
-// const DEFAULT_SYSTEM_PROMPT: &str = "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
-
-// If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.";
 
 #[http_component]
 fn handle(req: Request) -> Result<Response> {
@@ -31,45 +19,20 @@ fn handle(req: Request) -> Result<Response> {
 
 fn handle_completion(http_req: Request, _params: Params) -> Result<Response> {
   // parse the request
-  let api_req = GenerationRequestBuilder::new(http_req).build()?;
-  let model = api_req.get_model()?;
-  let options = api_req.get_params()?;
-  let prompt = get_completion_prompt(&api_req)?;
+  let api_req = GenerationRequest::try_from(http_req)?;
 
-  let infer_result = spin_sdk::llm::infer_with_options(model, &prompt, options)?;
+  let sdk_bldr = InferSdkBuilder::new()
+    .with_model(api_req.model)
+    .with_messages(api_req.messages)
+    .with_params(api_req.params)
+    //TODO: should be able to select this based on the model but I just want to get this done
+    .with_prompt_builder(Box::new(Llama2PromptBuilder {}));
+
+  let model = sdk_bldr.build_model()?;
+  let prompt = sdk_bldr.build_prompt()?;
+  let params = sdk_bldr.build_params();
+
+  let infer_result = spin_sdk::llm::infer_with_options(model, &prompt, params)?;
 
   GenerationResponseBuilder::new(infer_result).build()
-}
-
-fn get_completion_prompt(req: &GenerationRequest) -> Result<String> {
-  if req.messages.len() == 0 {
-    bail!("No messages provided");
-  }
-
-  let system_prompts: Vec<&GenerationMessage> = req.messages
-    .iter()
-    .filter(|p| p.role == "system")
-    .collect();
-
-  let user_prompts: Vec<&GenerationMessage> = req.messages
-    .iter()
-    .filter(|p| p.role == "user")
-    .collect();
-
-  if req.messages.len() > 2 || system_prompts.len() > 1 || user_prompts.len() > 1 {
-    bail!("Only one system and one user message are supported");
-  }
-
-  let system_prompt = system_prompts
-    .first()
-    .expect("No system prompt provided");
-  let user_prompt = user_prompts
-    .first()
-    .expect("No user prompt provided");
-
-  let prompt = format!(
-      "<s>[INST] <<SYS>>\n{}\n<</SYS>>\n\n{} [/INST]",
-      system_prompt.content, user_prompt.content
-  );
-  Ok(prompt)
 }
--- api/shared/src/lib.rs
+++ api/shared/src/lib.rs
@@ -1,4 +1,4 @@
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Result, bail};
 use serde::{Serialize, Deserialize};
 use spin_sdk::{http::{Request, Response}, llm::{InferencingModel, InferencingParams, InferencingResult}};
 
@@ -6,39 +6,17 @@ use spin_sdk::{http::{Request, Response}, llm::{InferencingModel, InferencingPar
 pub struct GenerationRequest {
     pub model: Option<String>,
     pub messages: Vec<GenerationMessage>,
-    pub options: Option<GenerationModelOptions>,
+    pub params: Option<GenerationModelParams>,
 }
 
-impl GenerationRequest {
-  // there is probably a more idiomatic way to do this
-  // but I'm tired of fighting the borrow checker
-  pub fn get_model(&self) -> Result<InferencingModel> {
-    match self.model.as_deref() {
-      Some("llama2-chat") => Ok(InferencingModel::Llama2Chat),
-      Some("codellama-instruct") => Ok(InferencingModel::CodellamaInstruct),
-      Some(other) => Ok(InferencingModel::Other(other)),
-      None => Err(anyhow!("No model specified")),
-    }
-  }
-
-  // there is probably a more idiomatic way to do this
-  // but I'm tired of fighting the borrow checker
-  pub fn get_params(&self) -> Result<InferencingParams> {
-      let defaults = InferencingParams::default();
-      match self.options {
-        Some(ref options) => {
-          Ok(InferencingParams {
-            max_tokens: options.max_tokens.unwrap_or(defaults.max_tokens),
-            repeat_penalty: options.repeat_penalty.unwrap_or(defaults.repeat_penalty),
-            repeat_penalty_last_n_token_count: options.repeat_penalty_last_n_token_count.unwrap_or(defaults.repeat_penalty_last_n_token_count),
-            temperature: options.temperature.unwrap_or(defaults.temperature),
-            top_k: options.top_k.unwrap_or(defaults.top_k),
-            top_p: options.top_p.unwrap_or(defaults.top_p),
-          })
-        },
-        None => Ok(InferencingParams::default()),
-      }
-  }
+#[derive(Debug, Serialize, Deserialize)]
+pub struct GenerationModelParams {
+  pub max_tokens: Option<u32>,
+  pub repeat_penalty: Option<f32>,
+  pub repeat_penalty_last_n_token_count: Option<u32>,
+  pub temperature: Option<f32>,
+  pub top_k: Option<u32>,
+  pub top_p: Option<f32>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -47,14 +25,13 @@ pub struct GenerationMessage {
   pub content: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct GenerationModelOptions {
-    pub max_tokens: Option<u32>,
-    pub repeat_penalty: Option<f32>,
-    pub repeat_penalty_last_n_token_count: Option<u32>,
-    pub temperature: Option<f32>,
-    pub top_k: Option<u32>,
-    pub top_p: Option<f32>,
+impl Clone for GenerationMessage {
+  fn clone(&self) -> Self {
+    Self {
+      role: self.role.clone(),
+      content: self.content.clone(),
+    }
+  }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -67,31 +44,6 @@ pub struct GenerationResponse {
 pub struct GenerationUsage {
     pub prompt_token_count: u32,
     pub generated_token_count: u32,
-}
-
-pub struct GenerationRequestBuilder {
-  req: Request,
-}
-impl GenerationRequestBuilder {
-  pub fn new(req: Request) -> Self {
-    Self { req }
-  }
-
-  pub fn build(self) -> Result<GenerationRequest> {
-    let body = match self.req.body() {
-      Some(b) => b.to_vec(),
-      None => Default::default(),
-    };
-    let request: GenerationRequest = serde_json::from_slice(&body)?;
-
-    //TODO: add behaviors here:
-    // - read options from config variables
-    // - read options from key-value store
-    // - override options from request
-    // - validation
-
-    Ok(request)
-  }
 }
 
 pub struct GenerationResponseBuilder {
@@ -118,5 +70,155 @@ impl GenerationResponseBuilder {
       .status(200)
       .body(Some(body.into()))
       .unwrap())
+  }
+}
+
+// dat new new
+impl TryFrom<Request> for GenerationRequest {
+  type Error = anyhow::Error;
+
+  fn try_from(req: Request) -> Result<Self> {
+    let body = match req.body() {
+      Some(b) => b.to_vec(),
+      None => Default::default(),
+    };
+    let request: GenerationRequest = serde_json::from_slice(&body)?;
+    Ok(request)
+  }
+}
+
+pub trait PromptBuilder {
+  fn build_prompt(&self, messages: Vec<GenerationMessage>) -> Result<String>;
+}
+
+pub mod llama2_prompt {
+    // based on -> https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/discussions/3
+
+    /* example single prompt:
+      <s>[INST] <<SYS>>
+      You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
+
+      If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
+      <</SYS>>
+
+      <prompt> [/INST]
+    */
+
+    /* example chat prompt:
+      <s>[INST] <<SYS>>
+      You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
+
+      If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
+      <</SYS>>
+
+      <prompt> [/INST] <answer> </s><s>[INST] <prompt-second> [/INST]
+    */
+
+  use super::*;
+
+  pub struct Llama2PromptBuilder {}
+
+  fn format_usr(content: &str) -> String {
+    format!("{} [/INST]\n\n", content)
+  }
+
+  fn format_asst(content: &str) -> String {
+    format!("{} </s><s>[INST]\n\n", content)
+  }
+
+  fn format_sys(content: &str) -> String {
+    format!(" <<SYS>>\n{}\n<</SYS>>\n\n", content)
+  }
+
+  impl PromptBuilder for Llama2PromptBuilder {
+    fn build_prompt(&self, messages: Vec<GenerationMessage>) -> Result<String> {
+      let mut prompt = String::new();
+
+      for i in 0..messages.len() {
+        let message = &messages[i];
+        match (i, message.role.as_str()) {
+          (0, "assistant") => bail!("First message cannot be from assistant"),
+          (0, "system") => prompt.push_str(&format_sys(&message.content)),
+          (_, "user") => prompt.push_str(&format_usr(&message.content)),
+          (_, "assistant") => prompt.push_str(&format_asst(&message.content)),
+          (_, "system") => bail!("System messages can only be the first message"),
+          (_, _) => bail!("Invalid role"),
+        }
+      }
+
+      Ok(prompt)
+    }
+  }
+}
+
+pub struct InferSdkBuilder {
+  model: Option<String>,
+  messages: Option<Vec<GenerationMessage>>,
+  params: Option<GenerationModelParams>,
+  prompt_builder: Option<Box<dyn PromptBuilder>>,
+}
+impl InferSdkBuilder {
+  pub fn new() -> Self {
+    Self {
+      model: None,
+      messages: None,
+      params: None,
+      prompt_builder: None
+    }
+  }
+
+  pub fn with_model(self, model: Option<String>) -> Self {
+    Self { model, ..self }
+  }
+
+  pub fn with_messages(self, messages: Vec<GenerationMessage>) -> Self {
+    Self { messages: Some(messages), ..self }
+  }
+
+  pub fn with_params(self, params: Option<GenerationModelParams>) -> Self {
+    Self { params, ..self }
+  }
+
+  pub fn with_prompt_builder(self, prompt_builder: Box<dyn PromptBuilder>) -> Self {
+    Self { prompt_builder: Some(prompt_builder), ..self }
+  }
+
+  pub fn build_params(self) -> InferencingParams {
+    let defaults = InferencingParams::default();
+    match self.params {
+      Some(p) => {
+        InferencingParams {
+          max_tokens: p.max_tokens.unwrap_or(defaults.max_tokens),
+          repeat_penalty: p.repeat_penalty.unwrap_or(defaults.repeat_penalty),
+          repeat_penalty_last_n_token_count: p.repeat_penalty_last_n_token_count.unwrap_or(defaults.repeat_penalty_last_n_token_count),
+          temperature: p.temperature.unwrap_or(defaults.temperature),
+          top_k: p.top_k.unwrap_or(defaults.top_k),
+          top_p: p.top_p.unwrap_or(defaults.top_p),
+        }
+      },
+      None => defaults,
+    }
+  }
+
+  pub fn build_model(&self) -> Result<InferencingModel<'static>> {
+    match self.model.as_deref() {
+      Some("llama2-chat") => Ok(InferencingModel::Llama2Chat),
+      Some("codellama-instruct") => Ok(InferencingModel::CodellamaInstruct),
+      // I have fought with this for hours and I don't understand how to return the inferencing model enum since its a temporary value
+      // other => Ok(InferencingModel::Other(other)),
+      _ => Err(anyhow!("Support for other models is not yet implemented")),
+    }
+  }
+
+  pub fn build_prompt(&self) -> Result<String> {
+    match self.prompt_builder.as_deref() {
+      Some(pb) => {
+        match self.messages.as_deref() {
+          Some(messages) => pb.build_prompt(messages.to_vec()),
+          None => Err(anyhow!("Messages must be provided when using a prompt builder")),
+        }
+      },
+      None => Err(anyhow!("Prompt builder must be provided")),
+    }
   }
 }
